d2d87efcceff8601ce547e6e5ce7d6faac7f61c1
Miguel Angel Zambrano <angel245687@gmail.com>
2026-01-03 00:21:52 -0300

    creo nav y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Home from './components/pages/Home'
+import Menu from './components/shared/Menu'
+import Footer from './components/shared/Footer'
 
 function App() {
 
   return (
     <>
+    
+    <BrowserRouter>
+    <Menu></Menu>
+    <main>
+      <Routes>
+    <Route path='/' element={<Home></Home>} ></Route>
+    </Routes>
+    </main>
+    <Footer></Footer>
+    </BrowserRouter>
+    
     </>
   )
 }
